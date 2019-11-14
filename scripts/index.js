97d618c70c5dbc755aaf4393b1a378a7bca0c8f5
sebastian07leal <sebastianleal2016@gmail.com>
2019-11-14 15:56:12 -0500

    Cambio a zoom de imagen

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
 
   document.addEventListener('DOMContentLoaded', function() {
     
-    /**Este codigo se encarga de controlar la animacion de sidenav*/
     const elems = document.querySelectorAll('.sidenav');
-    /**Si los scripts de materialize no se encientra iniciados
-     * No se podra hacaer referencia a "M" dado que no estara 
-     * instanciado.
-     */
+    const img = document.querySelectorAll('.materialboxed');
+
     const instances = M.Sidenav.init(elems);
+    const inst = M.Materialbox.init(img);
+
 
     
   });
